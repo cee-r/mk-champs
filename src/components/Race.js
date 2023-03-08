e37a02dacb68c686873fc3739ecc5bb93c398148
Celina Respall <celina.respall@utoronto.ca>
2023-03-08 15:05:35 -0500

    Add concept

---
+++ src/components/Race.js
@@ -0,0 +1,31 @@
+import Players from "../data/Players"
+import Player from "./Player"
+
+function Race(props) {
+
+  const filterByRace = Players.filter(player => player.race === props.id);
+
+  const playerList = filterByRace.map(player => {
+    return (
+      <Player key={player.id} name={player.name} portrait={player.character} place={player.finish} race={player.race} />
+    )
+  })
+
+  return (
+    <div className="list">
+        {props.date === 'N/A' ? 'no race' : (
+          <div className="item">
+            <div className="race">
+              <h2>{props.title}</h2>
+              <span className="date">{props.date}</span>
+            </div>
+            <ul className="players">
+              {playerList}
+            </ul>
+          </div>
+        )}
+    </div>
+  );
+}
+
+export default Race;
---
+++ src/data/Races.js
@@ -0,0 +1,64 @@
+const Races = [
+  {
+    id: 1,
+    date: '2022-08-12',
+    title: 'Race I',
+    description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC' 
+  },
+  {
+    id: 2,
+    date: '2022-12-13',
+    title: 'Race II',
+    description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC'
+  },
+  {
+    id: 3,
+    date: 'N/A',
+    title: 'Race III',
+    description: 'N/A'
+  },
+  {
+    id: 4,
+    date: 'N/A',
+    title: 'Race IV',
+    description: 'N/A'
+  },
+  {
+    id: 5,
+    date: 'N/A',
+    title: 'Race V',
+    description: 'N/A'
+  },
+  {
+    id: 6,
+    date: 'N/A',
+    title: 'Race VI',
+    description: 'N/A'
+  },
+  {
+    id: 7,
+    date: 'N/A',
+    title: 'Race VII',
+    description: 'N/A'
+  },
+  {
+    id: 8,
+    date: 'N/A',
+    title: 'Race VIII',
+    description: 'N/A'
+  },
+  {
+    id: 9,
+    date: 'N/A',
+    title: 'Race IX',
+    description: 'N/A'
+  }, 
+  {
+    id: 10,
+    date: 'N/A',
+    title: 'Race X',
+    description: 'N/A'
+  }
+];
+
+export default Races;
