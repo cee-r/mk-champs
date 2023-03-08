2800aa12ffae6425adda0668cc8f6402f1d10f3f
Celina Respall <celina.respall@utoronto.ca>
2023-03-08 15:50:34 -0500

    Add class

--- src/data/Races.js
+++ src/data/Races.js
@@ -3,61 +3,71 @@ const Races = [
     id: 1,
     date: '2022-08-12',
     title: 'Race I',
-    description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC' 
+    description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC',
+    colors: 'bowser-jr'
   },
   {
     id: 2,
     date: '2022-12-13',
     title: 'Race II',
-    description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC'
+    description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC',
+    colors: 'baby-daisy'
   },
   {
     id: 3,
     date: 'N/A',
     title: 'Race III',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   },
   {
     id: 4,
     date: 'N/A',
     title: 'Race IV',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   },
   {
     id: 5,
     date: 'N/A',
     title: 'Race V',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   },
   {
     id: 6,
     date: 'N/A',
     title: 'Race VI',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   },
   {
     id: 7,
     date: 'N/A',
     title: 'Race VII',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   },
   {
     id: 8,
     date: 'N/A',
     title: 'Race VIII',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   },
   {
     id: 9,
     date: 'N/A',
     title: 'Race IX',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   }, 
   {
     id: 10,
     date: 'N/A',
     title: 'Race X',
-    description: 'N/A'
+    description: 'N/A',
+    colors: ''
   }
 ];
 
