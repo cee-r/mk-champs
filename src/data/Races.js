899d4ec355d06a52e7664fdb3b6893acdf2a25c7
cee-r <cr.repository@gmail.com>
2025-08-18 16:41:44 -0400

    Set up colors- need to pick better values

--- src/data/Races.js
+++ src/data/Races.js
@@ -4,14 +4,14 @@ const Races = [
     date: '2022-08-12',
     title: 'Race I',
     description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC',
-    colors: 'bowser-jr'
+    colors: 'green'
   },
   {
     id: 2,
     date: '2022-12-13',
     title: 'Race II',
     description: 'Free For All: 4 Heats / 4 Random Tracks / 150CC. Final 4 / 4 Random Tracks / 200CC',
-    colors: 'baby-daisy'
+    colors: 'orange'
   },
   {
     id: 3,
@@ -41,34 +41,34 @@ const Races = [
     description: 'N/A',
     colors: ''
   },
-  {
-    id: 7,
-    date: 'N/A',
-    title: 'Race VII',
-    description: 'N/A',
-    colors: ''
-  },
-  {
-    id: 8,
-    date: 'N/A',
-    title: 'Race VIII',
-    description: 'N/A',
-    colors: ''
-  },
-  {
-    id: 9,
-    date: 'N/A',
-    title: 'Race IX',
-    description: 'N/A',
-    colors: ''
-  }, 
-  {
-    id: 10,
-    date: 'N/A',
-    title: 'Race X',
-    description: 'N/A',
-    colors: ''
-  }
+  // {
+  //   id: 7,
+  //   date: 'N/A',
+  //   title: 'Race VII',
+  //   description: 'N/A',
+  //   colors: ''
+  // },
+  // {
+  //   id: 8,
+  //   date: 'N/A',
+  //   title: 'Race VIII',
+  //   description: 'N/A',
+  //   colors: ''
+  // },
+  // {
+  //   id: 9,
+  //   date: 'N/A',
+  //   title: 'Race IX',
+  //   description: 'N/A',
+  //   colors: ''
+  // }, 
+  // {
+  //   id: 10,
+  //   date: 'N/A',
+  //   title: 'Race X',
+  //   description: 'N/A',
+  //   colors: ''
+  // }
 ];
 
 export default Races;
